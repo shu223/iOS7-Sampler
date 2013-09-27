9559d867cc31af18c5d69c089b00c646b68eead1
Andrew Frederick <andrew.frederick@mutualmobile.com>
2013-09-27 13:51:07 -0500

    Changed indent to tabs, fixed issue where canceling the browser view wouldn't show the launch buttons

--- iOS7Sampler/SampleViewControllers/MultipeerConnectivityViewController.m
+++ iOS7Sampler/SampleViewControllers/MultipeerConnectivityViewController.m
@@ -42,65 +42,67 @@ static NSString * const kMessageKey = @"message";
 @implementation MultipeerConnectivityViewController
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
-  _messageTextField.hidden = YES;
-  _sendMessageButton.hidden = YES;
-  _activityView.hidden = YES;
+    [super viewDidLoad];
+    _messageTextField.hidden = YES;
+    _sendMessageButton.hidden = YES;
+    _activityView.hidden = YES;
 }
 
 - (IBAction)launchBrowserPressed:(id)sender {
-  _peerID = [[MCPeerID alloc] initWithDisplayName:@"Browser Name"];
-  _session = [[MCSession alloc] initWithPeer:_peerID];
-  _session.delegate = self;
-  _browserView = [[MCBrowserViewController alloc] initWithServiceType:kServiceName
-                                                              session:_session];
-  _browserView.delegate = self;
-  [self presentViewController:_browserView animated:YES completion:nil];
-
-  _launchAdvertiserButton.hidden = YES;
-  _launchBrowserButton.hidden = YES;
+    _peerID = [[MCPeerID alloc] initWithDisplayName:@"Browser Name"];
+    _session = [[MCSession alloc] initWithPeer:_peerID];
+    _session.delegate = self;
+    _browserView = [[MCBrowserViewController alloc] initWithServiceType:kServiceName
+                                                                session:_session];
+    _browserView.delegate = self;
+    [self presentViewController:_browserView animated:YES completion:nil];
+    
+    _launchAdvertiserButton.hidden = YES;
+    _launchBrowserButton.hidden = YES;
 }
 
 - (IBAction)launchAdvertiser:(id)sender {
-  _peerID = [[MCPeerID alloc] initWithDisplayName:@"Advertiser Name"];
-  _session = [[MCSession alloc] initWithPeer:_peerID];
-  _session.delegate = self;
-  _advertiserAssistant = [[MCAdvertiserAssistant alloc] initWithServiceType:kServiceName
-                                                              discoveryInfo:nil
-                                                                    session:_session];
-  [_advertiserAssistant start];
-  
-  _launchAdvertiserButton.hidden = YES;
-  _launchBrowserButton.hidden = YES;
-  _activityView.hidden = NO;
+    _peerID = [[MCPeerID alloc] initWithDisplayName:@"Advertiser Name"];
+    _session = [[MCSession alloc] initWithPeer:_peerID];
+    _session.delegate = self;
+    _advertiserAssistant = [[MCAdvertiserAssistant alloc] initWithServiceType:kServiceName
+                                                                discoveryInfo:nil
+                                                                      session:_session];
+    [_advertiserAssistant start];
+    
+    _launchAdvertiserButton.hidden = YES;
+    _launchBrowserButton.hidden = YES;
+    _activityView.hidden = NO;
 }
 
 - (IBAction)sendMessageButtonPressed:(id)sender {
-  NSString *message = _messageTextField.text;
-  NSDictionary *dataDict = @{ kMessageKey : message };
-  NSData *data = [NSPropertyListSerialization dataWithPropertyList:dataDict
-                                                            format:NSPropertyListBinaryFormat_v1_0
-                                                           options:0
-                                                             error:NULL];
-  NSError *error;
-  [self.session sendData:data
-                 toPeers:[_session connectedPeers]
-                withMode:MCSessionSendDataReliable
-                   error:&error];
+    NSString *message = _messageTextField.text;
+    NSDictionary *dataDict = @{ kMessageKey : message };
+    NSData *data = [NSPropertyListSerialization dataWithPropertyList:dataDict
+                                                              format:NSPropertyListBinaryFormat_v1_0
+                                                             options:0
+                                                               error:NULL];
+    NSError *error;
+    [self.session sendData:data
+                   toPeers:[_session connectedPeers]
+                  withMode:MCSessionSendDataReliable
+                     error:&error];
 }
 
 #pragma mark - MCBrowserViewControllerDelegate
 
 - (void)browserViewControllerDidFinish:(MCBrowserViewController *)browserViewController {
-  [self dismissViewControllerAnimated:YES completion:^{
-    [_browserView.browser stopBrowsingForPeers];
-  }];
+    [self dismissViewControllerAnimated:YES completion:^{
+        [_browserView.browser stopBrowsingForPeers];
+    }];
 }
 
 - (void)browserViewControllerWasCancelled:(MCBrowserViewController *)browserViewController {
-  [self dismissViewControllerAnimated:YES completion:^{
-    [_browserView.browser stopBrowsingForPeers];
-  }];
+    [self dismissViewControllerAnimated:YES completion:^{
+        [_browserView.browser stopBrowsingForPeers];
+        _launchAdvertiserButton.hidden = NO;
+        _launchBrowserButton.hidden = NO;
+    }];
 }
 
 #pragma mark - MCSessionDelegate
@@ -109,46 +111,50 @@ static NSString * const kMessageKey = @"message";
 // elements you must perform the actions on the main queue.
 
 - (void)session:(MCSession *)session peer:(MCPeerID *)peerID didChangeState:(MCSessionState)state {
-  switch (state) {
-    case MCSessionStateConnected: {
-      dispatch_async(dispatch_get_main_queue(), ^{
-        _messageTextField.hidden = NO;
-        _sendMessageButton.hidden = NO;
-        _activityView.hidden = YES;
-      });
-      break;
+    switch (state) {
+        case MCSessionStateConnected: {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                _messageTextField.hidden = NO;
+                _sendMessageButton.hidden = NO;
+                _activityView.hidden = YES;
+            });
+            
+            // This line only necessary for the advertiser. We want to stop advertising our services
+            // to other browsers when we successfully connect to one.
+            [_advertiserAssistant stop];
+            break;
+        }
+        case MCSessionStateNotConnected: {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                _launchAdvertiserButton.hidden = NO;
+                _launchBrowserButton.hidden = NO;
+                _messageTextField.hidden = YES;
+                _sendMessageButton.hidden = YES;
+            });
+            break;
+        }
+        default:
+            break;
     }
-    case MCSessionStateNotConnected: {
-      dispatch_async(dispatch_get_main_queue(), ^{
-        _launchAdvertiserButton.hidden = NO;
-        _launchBrowserButton.hidden = NO;
-        _messageTextField.hidden = YES;
-        _sendMessageButton.hidden = YES;
-      });
-      break;
-    }
-    default:
-      break;
-  }
 }
 
 - (void)session:(MCSession *)session didReceiveData:(NSData *)data fromPeer:(MCPeerID *)peerID {
-  NSPropertyListFormat format;
-  NSDictionary *receivedData = [NSPropertyListSerialization propertyListWithData:data
-                                                                         options:0
-                                                                          format:&format
-                                                                           error:NULL];
-  NSString *message = receivedData[kMessageKey];
-  if ([message length]) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-      UIAlertView *messageAlert = [[UIAlertView alloc] initWithTitle:@"Received message"
-                                                             message:message
-                                                            delegate:self
-                                                   cancelButtonTitle:@"OK"
-                                                   otherButtonTitles:nil];
-      [messageAlert show];
-    });
-  }
+    NSPropertyListFormat format;
+    NSDictionary *receivedData = [NSPropertyListSerialization propertyListWithData:data
+                                                                           options:0
+                                                                            format:&format
+                                                                             error:NULL];
+    NSString *message = receivedData[kMessageKey];
+    if ([message length]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIAlertView *messageAlert = [[UIAlertView alloc] initWithTitle:@"Received message"
+                                                                   message:message
+                                                                  delegate:self
+                                                         cancelButtonTitle:@"OK"
+                                                         otherButtonTitles:nil];
+            [messageAlert show];
+        });
+    }
 }
 
 // Required MCSessionDelegate protocol methods but are unused in this application.
@@ -157,14 +163,14 @@ static NSString * const kMessageKey = @"message";
     didStartReceivingResourceWithName:(NSString *)resourceName
                              fromPeer:(MCPeerID *)peerID
                          withProgress:(NSProgress *)progress {
-  
+    
 }
 
 - (void)     session:(MCSession *)session
     didReceiveStream:(NSInputStream *)stream
             withName:(NSString *)streamName
             fromPeer:(MCPeerID *)peerID {
-  
+    
 }
 
 - (void)                       session:(MCSession *)session
@@ -172,7 +178,7 @@ static NSString * const kMessageKey = @"message";
                               fromPeer:(MCPeerID *)peerID
                                  atURL:(NSURL *)localURL
                              withError:(NSError *)error {
-  
+    
 }
 
 @end
