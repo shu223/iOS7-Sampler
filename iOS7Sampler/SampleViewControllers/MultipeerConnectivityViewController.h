ce7eeba3f0774567a9f372040428f411f0bd4dea
Andrew Frederick <andrew.frederick@mutualmobile.com>
2013-09-27 13:38:31 -0500

    Added MultipeerConnectivity view controller

---
+++ iOS7Sampler/SampleViewControllers/MultipeerConnectivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  MultipeerConnectivityViewController.h
+//  iOS7Sampler
+//
+//  Created by Andrew Frederick on 9/27/13.
+//  Copyright (c) 2013 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MultipeerConnectivityViewController : UIViewController
+
+@end
